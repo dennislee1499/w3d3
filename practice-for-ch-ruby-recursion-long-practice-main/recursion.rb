5a9012ca674946bc855a1c6b181c4a9f3c930b37
Dennis Lee <dennislee1499@gmail.com>
2023-07-12 14:00:08 -0400

    finish warmup

--- practice-for-ch-ruby-recursion-long-practice-main/recursion.rb
+++ practice-for-ch-ruby-recursion-long-practice-main/recursion.rb
@@ -8,13 +8,23 @@ def range_iterative(first, last)
 end
 
 def range_recursive(first, last)
-    return [] if last < first
-    return first if arr.length == 1
-    arr = (first...last).to_a
-    arr[0]
+    return [] if last <= first
+    return first if first == last - 1
+    
+    range_recursive(first, last-1) + (last-1)
+
+    # arr = (first...last).to_a
+    # arr.first + range_recursive(first + 1, last)
+
+
 end
 
 # []   
-# [1] return 1
-# [1, 2] return 3
-# [1, 2, 3] return 6
+# range_recursive (1,1) # [1] return 1 
+#range_recursive(1,2) return 1 
+# range_recursive(1,3) [1, 2] return 6
+# 
+
+#
+
+
